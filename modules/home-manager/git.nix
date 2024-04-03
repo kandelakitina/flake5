0986a4fda47e00bc7f515ca38a7a7be9c8374390
boticelli <kandelakitina@gmail.com>
2024-04-03 20:57:07 +0300

    Add git

---
+++ modules/home-manager/git.nix
@@ -0,0 +1,98 @@
+{config, pkgs, ...}: {
+  programs.git = {
+    enable = true;
+    userName = "boticelli";
+    userEmail = "kandelakitina@gmail.com";
+
+    # signing = {
+    #   signByDefault = true;
+    #   key = "D528 BD50 F4E9 F031 AACB 1F7A 9833 E49C 848D 6C90";
+    # };
+
+    extraConfig = {
+      core = {
+        editor = "hx";
+        pager = "delta";
+      };
+
+      color = {
+        ui = true;
+      };
+
+      interactive = {
+        diffFitler = "delta --color-only";
+      };
+
+      delta = {
+        enable = true;
+        navigate = true;
+        light = false;
+        side-by-side = false;
+        options.syntax-theme = "catppuccin";
+      };
+
+      pull = {
+        ff = "only";
+      };
+
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+
+      init = {
+        defaultBranch = "init";
+      };
+    };
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --color-only --dark --paging=never";
+          useConfig = false;
+        };
+      };
+      gui.theme = {
+        lightTheme = false;
+        # activeBorderColor = ["#${config.colorscheme.colors.base0B}" "bold"];
+        # inactiveBorderColor = ["#${config.colorscheme.colors.base05}"];
+        # optionsTextColor = ["#${config.colorscheme.colors.base0D}"];
+        # selectedLineBgColor = ["#${config.colorscheme.colors.base02}"];
+        # selectedRangeBgColor = ["#${config.colorscheme.colors.base02}"];
+        # cherryPickedCommitBgColor = ["#${config.colorscheme.colors.base0C}"];
+        # cherryPickedCommitFgColor = ["#${config.colorscheme.colors.base0D}"];
+        # unstagedChangesColor = ["#${config.colorscheme.colors.base08}"];
+      };
+      customCommands = [
+        {
+          key = "W";
+          command = "git commit -m '{{index .PromptResponses 0}}' --no-verify";
+          description = "commit without verification";
+          context = "global";
+          subprocess = true;
+        }
+        {
+          key = "S";
+          command = "git commit -m '{{index .PromptResponses 0}}' --no-gpg-sign";
+          description = "commit without gpg signing";
+          context = "global";
+          subprocess = true;
+        }
+      ];
+    };
+  };
+  
+  home.packages = with pkgs; [
+    delta
+  ];
+  
+  home.shellAliases = {
+    g = "git";
+    lg = "lazygit";
+  };
+  
+}
