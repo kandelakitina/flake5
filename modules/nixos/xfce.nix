6ca9b0f87590e6d99fb8e7c14846e15dc118370f
boticelli <kandelakitina@gmail.com>
2024-04-03 17:54:18 +0300

    Add xfce 3

--- modules/nixos/xfce.nix
+++ modules/nixos/xfce.nix
@@ -1,11 +1,21 @@
 {
-  services.xserver.desktopManager.xfce.enable = true;
-  services.xserver.displayManager.defaultSession = "xfce";
-  services.picom = {
-    enable = true;
-    fade = true;
-    inactiveOpacity = 0.9;
-    shadow = true;
-    fadeDelta = 4;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        xfce.enable = true;
+        defaultSession = "xfce";
+      };
+      desktopManager = {
+        xfce.enable = true;
+      };
+    };
+    picom = {
+	  enable = true;
+	  fade = true;
+	  inactiveOpacity = 0.9;
+	  shadow = true;
+	  fadeDelta = 4;
+	};
   };
 }
