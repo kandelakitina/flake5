d571556bbf2f7944fea56f16c73291e926c82c77
boticelli <kandelakitina@gmail.com>
2024-04-04 10:05:19 +0300

    Add local and make hx default editor

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -105,4 +105,31 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
+
+  i18n = {
+    defaultLocale = lib.mkDefault "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "en_GB.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "en_GB.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
+  };
+
+  time.timeZone = "Europe/Moscow";
+
+  services.xserver = {
+    layout = "us,ru";
+    xkbOptions = "grp:alt_shift_toggle,caps:escape";
+  };
+
+  console.keyMap = "us";
+
+  environment.variables.EDITOR = "hx";
+
 }
