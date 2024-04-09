35594cfc8e45bf2fadfb26e728d3912b3052e42a
boticelli <kandelakitina@gmail.com>
2024-04-09 10:41:25 +0300

    Update zellij theme

--- modules/home-manager/zellij/themes.nix
+++ modules/home-manager/zellij/themes.nix
@@ -1,32 +1,26 @@
 {colorScheme}: let
-  backgroundDeepBlue = "#${colorScheme.palette.base00}"; # Deep Blue (background)
-  black = "#${colorScheme.palette.base01}"; # Black
-  lightBlack = "#${colorScheme.palette.base02}"; # Light Black
-  magenta = "#${colorScheme.palette.base03}"; # Magenta (purple)
-  lightMagenta = "#${colorScheme.palette.base07}"; # Light Magenta (purple)
-  foregroundOffWhite = "#${colorScheme.palette.base04}"; # Off White (foreground)
-  pureWhite = "#${colorScheme.palette.base05}"; # White (pure)
-  defaultWhite = "#${colorScheme.palette.base06}"; # White (default)
-  red = "#${colorScheme.palette.base08}"; # Red
-  yellow = "#${colorScheme.palette.base09}"; # Yellow
-  lightYellow = "#${colorScheme.palette.base0A}"; # Light Yellow
-  green = "#${colorScheme.palette.base0B}"; # Green
-  cyan = "#${colorScheme.palette.base0C}"; # Cyan
-  blue = "#${colorScheme.palette.base0D}"; # Blue
-  lightRed = "#${colorScheme.palette.base0E}"; # Light Red
-  lightBlue = "#${colorScheme.palette.base0F}"; # Light Blue
+  background = "#${colorScheme.palette.base00}";
+  darkGrey = "#${colorScheme.palette.base01}";
+  purple = "#${colorScheme.palette.base03}";
+  grey = "#${colorScheme.palette.base04}";
+  white = "#${colorScheme.palette.base05}";
+  red = "#${colorScheme.palette.base08}";
+  yellow = "#${colorScheme.palette.base09}";
+  green = "#${colorScheme.palette.base0B}";
+  cyan = "#${colorScheme.palette.base0C}";
+  blue = "#${colorScheme.palette.base0D}";
 in {
   "nix-${colorScheme.slug}" = {
-    fg = lightBlack; # Buttons' colors
+    fg = grey; # Buttons' colors
     red = yellow; # Keybinds on buttons
-    black = backgroundDeepBlue; # Bars background and "LOCK" etc
-    green = magenta; # Panes' borders, tabs button, secondary keys in Tips
-    bg = magenta; # ???
-    yellow = yellow; 
+    black = background; # Bars background and "LOCK" etc
+    green = purple; # Panes' borders, tabs button, secondary keys in Tips
+    bg = purple; # ???
+    yellow = yellow;
     blue = blue;
-    magenta = magenta;
+    magenta = purple;
     cyan = red;
-    white = lightBlack;
+    white = grey;
     orange = yellow;
   };
 }
