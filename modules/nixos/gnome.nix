88a85006bc93aee55b1bd1777674355a08279b79
boticelli <kandelakitina@gmail.com>
2024-04-30 00:26:42 +0300

    Update flake lock

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,40 +1,32 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   services = {
+    displayManager = { defaultSession = "gnome"; };
     xserver = {
       enable = true;
-      displayManager = {
-        gdm.enable = true;
-        defaultSession = "gnome";
-      };
-      desktopManager = {
-        gnome.enable = true;
-      };
+      displayManager = { gdm.enable = true; };
+      desktopManager = { gnome.enable = true; };
     };
   };
 
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-photos
-      gnome-tour
-      gedit # text editor
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      # epiphany # web browser
-      geary # email reader
-      gnome-characters
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-      yelp # Help view
-      gnome-contacts
-      gnome-initial-setup
-    ]);
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gedit # text editor
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    # epiphany # web browser
+    geary # email reader
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp # Help view
+    gnome-contacts
+    gnome-initial-setup
+  ]);
 
   programs.dconf.enable = true;
-  environment.systemPackages = with pkgs; [
-    gnome.gnome-tweaks
-  ];
+  environment.systemPackages = with pkgs; [ gnome.gnome-tweaks ];
 }
--- modules/nixos/xfce.nix
+++ modules/nixos/xfce.nix
@@ -1,21 +1,17 @@
 {
   services = {
+    displayManager = { defaultSession = "xfce"; };
     xserver = {
       enable = true;
-      displayManager = {
-        lightdm.enable = true;
-        defaultSession = "xfce";
-      };
-      desktopManager = {
-        xfce.enable = true;
-      };
+      displayManager = { lightdm.enable = true; };
+      desktopManager = { xfce.enable = true; };
     };
     picom = {
-	  enable = true;
-	  fade = true;
-	  inactiveOpacity = 0.9;
-	  shadow = true;
-	  fadeDelta = 4;
-	};
+      enable = true;
+      fade = true;
+      inactiveOpacity = 0.9;
+      shadow = true;
+      fadeDelta = 4;
+    };
   };
 }
