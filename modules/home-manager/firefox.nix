d1ad49f4b6a45f277e8440e5541c58ed26084633
boticelli <kandelakitina@gmail.com>
2024-04-30 23:31:03 +0300

    Update firefox - gnome theme

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -9,8 +9,8 @@
     };
   };
 
-  # home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source =
-  #   inputs.firefox-gnome-theme;
+  home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source =
+    inputs.firefox-gnome-theme;
 
   programs.firefox = {
     enable = true;
@@ -35,6 +35,19 @@
         undoclosetabbutton
         browserpass
       ];
+
+      settings = {
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+        # For Firefox GNOME theme:
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.tabs.drawInTitlebar" = true;
+        "svg.context-properties.content.enabled" = true;
+      };
+      userChrome = ''
+        @import "firefox-gnome-theme/userChrome.css";
+        @import "firefox-gnome-theme/theme/colors/dark.css"; 
+      '';
 
       # settings = {
       #   "browser.uidensity" = 0;
