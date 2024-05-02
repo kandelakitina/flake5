d980356f2c112bbd2433e3ae66fb310529107053
boticelli <kandelakitina@gmail.com>
2024-05-02 21:27:27 +0300

    add persist to v2ray

--- modules/nixos/v2ray.nix
+++ modules/nixos/v2ray.nix
@@ -1,4 +1,8 @@
 {
   # v2rayA - Proxy server web front end
   services.v2raya.enable = true;
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [ "/var/v2raya" ];
+  };
 }
