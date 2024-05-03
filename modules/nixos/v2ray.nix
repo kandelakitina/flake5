3026462da515e9a79e5caccefc68cc7346abdb24
boticelli <kandelakitina@gmail.com>
2024-05-03 10:07:57 +0300

    Revert "add persist to v2ray"
    This reverts commit d980356f2c112bbd2433e3ae66fb310529107053.

--- modules/nixos/v2ray.nix
+++ modules/nixos/v2ray.nix
@@ -1,8 +1,4 @@
 {
   # v2rayA - Proxy server web front end
   services.v2raya.enable = true;
-  environment.persistence."/persist" = {
-    hideMounts = true;
-    directories = [ "/var/v2raya" ];
-  };
 }
