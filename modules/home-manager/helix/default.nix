9daeda99f83f52d4d3efffe88a516a2dfa703179
boticelli <kandelakitina@gmail.com>
2024-06-30 17:14:36 +0300

    Add space u w binding to helix

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -96,8 +96,8 @@ in {
 
       keys.normal.space.u = {
         f = ":format"; # format using LSP formatter
-        w = ":set whitespace.render all";
-        W = ":set whitespace.render none";
+        s = ":toggle whitespace.render all none"; # show special symbols
+        w = ":toggle soft-wrap.enable"; # toggle line wrap
       };
     };
 
