7cc2e5654ce9582c6a01d702d43b2d07c2b7408f
boticelli <kandelakitina@gmail.com>
2024-07-05 22:43:39 +0300

    Comment out swapDevices from hardwareConfig.nix

--- hosts/beelink/hardware-configuration.nix
+++ hosts/beelink/hardware-configuration.nix
@@ -17,7 +17,8 @@
     options iwlwifi disable_11ax=1
   '';
 
-  swapDevices = [{ device = "/dev/disk/by-partlabel/disk-main-swap"; }];
+  # Seems to conflict with brfts.nix
+  # swapDevices = [{ device = "/dev/disk/by-partlabel/disk-main-swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
