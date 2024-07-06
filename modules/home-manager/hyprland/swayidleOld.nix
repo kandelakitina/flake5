1073c465d0d63a4ef63b52f70add2e8d8eaf1a35
boticelli <kandelakitina@gmail.com>
2024-07-07 00:08:12 +0300

    Try: Update swayidle

---
+++ modules/home-manager/hyprland/swayidleOld.nix
@@ -0,0 +1,35 @@
+{ pkgs, config, ... }:
+let
+  lock =
+    # "${pkgs.swaylock-effects}/bin/swaylock --daemonize --clock --indicator --grace 10";
+    "${pkgs.swaylock-effects}/bin/swaylock --daemonize --clock --indicator";
+  hyprlandPkg = config.wayland.windowManager.hyprland.package;
+in {
+  services.swayidle = {
+    enable = true;
+    events = [
+      # executes command before systemd puts the computer to sleep.
+      {
+        event = "before-sleep";
+        command = lock;
+      }
+      # executes command when logind signals that the session should be locked
+      {
+        event = "lock";
+        command = lock;
+      }
+    ];
+    timeouts = [
+      {
+        timeout = 5 * 60;
+        command = lock;
+      }
+      {
+        timeout = 15 * 60;
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+        # command = "${hyprlandPkg}/bin/hyprctl dispatch dpms off";
+        # resumeCommand = "${hyprlandPkg}/bin/hyprctl dispatch dpms on";
+      }
+    ];
+  };
+}
