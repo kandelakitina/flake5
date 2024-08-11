ca1d6666a26ffae41147b1f60d17f129f25bc811
boticelli <kandelakitina@gmail.com>
2024-08-11 23:39:32 +0300

    Add autolock if yubikey removed

--- modules/nixos/pam.nix
+++ modules/nixos/pam.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }: {
   services = {
-    pcscd.enable = true;
+    # pcscd.enable = true;
     udev.packages = with pkgs; [ yubikey-personalization ];
-    dbus.packages = [ pkgs.gcr ];
+    # dbus.packages = [ pkgs.gcr ];
+  };
+
+  security.pam.u2f = {
+    enable = true;
+    cue = true;
   };
 
   security.pam.services = {
-    swaylock = { u2fAuth = true; };
-    login = { u2fAuth = true; };
-    sudo = { u2fAuth = true; };
+    swaylock.u2fAuth = true;
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+    sddm.u2fAuth = true;
   };
 
   # security.pam.yubico = {
@@ -17,4 +23,13 @@
   #   mode = "challenge-response";
   #   id = [ "25940960" ];
   # };
+
+  services.udev.extraRules = ''
+    ACTION=="remove",\
+     ENV{ID_BUS}=="usb",\
+     ENV{ID_MODEL_ID}=="0407",\
+     ENV{ID_VENDOR_ID}=="1050",\
+     ENV{ID_VENDOR}=="Yubico",\
+     RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
 }
