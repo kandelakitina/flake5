d34c5216a75dc4b901d7293964fe7ea717f65403
boticelli <kandelakitina@gmail.com>
2024-10-30 11:32:43 +0300

    Fixed swaylock and swayidle (seemingly)

--- modules/home-manager/hyprland/swayidle.nix
+++ modules/home-manager/hyprland/swayidle.nix
@@ -1,36 +1,20 @@
-{ pkgs, config, ... }:
-let
-  lock =
-    # "${pkgs.swaylock-effects}/bin/swaylock --daemonize --clock --indicator --grace 10";
-    # "${pkgs.swaylock-effects}/bin/swaylock --daemonize --clock --indicator";
-    "${pkgs.swaylock}/bin/swaylock --daemonize --clock --indicator";
-  hyprlandPkg = config.wayland.windowManager.hyprland.package;
-in {
+{ pkgs, lib, config, ... }: {
   services.swayidle = {
     enable = true;
+    package = pkgs.swayidle;
     events = [
-      # executes command before systemd puts the computer to sleep.
       {
         event = "before-sleep";
-        command = lock;
+        command = "swaylock";
       }
-      # executes command when logind signals that the session should be locked
       {
         event = "lock";
-        command = lock;
+        command = "swaylock";
       }
     ];
-    timeouts = [
-      {
-        timeout = 5 * 60;
-        command = lock;
-      }
-      {
-        timeout = 15 * 60;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-        # command = "${hyprlandPkg}/bin/hyprctl dispatch dpms off";
-        # resumeCommand = "${hyprlandPkg}/bin/hyprctl dispatch dpms on";
-      }
-    ];
+    timeouts = [{
+      timeout = 60;
+      command = "swaylock -fF";
+    }];
   };
 }
