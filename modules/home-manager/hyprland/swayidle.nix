150feb406881ef815f4e7188cb04a3994b8a063b
boticelli <kandelakitina@gmail.com>
2024-12-03 20:37:18 +0300

    Update swayidle

--- modules/home-manager/hyprland/swayidle.nix
+++ modules/home-manager/hyprland/swayidle.nix
@@ -1,20 +1,52 @@
 { pkgs, lib, config, ... }: {
   services.swayidle = {
     enable = true;
-    package = pkgs.swayidle;
     events = [
       {
         event = "before-sleep";
-        command = "swaylock";
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize";
       }
       {
         event = "lock";
-        command = "swaylock";
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize --grace 0";
+      }
+      {
+        event = "unlock";
+        command = "pkill -SIGUSR1 swaylock";
+      }
+      {
+        event = "after-resume";
+        command = ''swaymsg "output * dpms on"'';
       }
     ];
-    timeouts = [{
-      timeout = 60;
-      command = "swaylock -fF";
-    }];
+    timeouts = [
+      {
+        timeout = 1800;
+        command = "${pkgs.swaylock-effects}/bin/swaylock --daemonize";
+      }
+      {
+        timeout = 2000;
+        command = ''swaymsg "output * dpms off"'';
+        resumeCommand = ''swaymsg "output * dpms on"'';
+      }
+    ];
   };
+  # services.swayidle = {
+  #   enable = true;
+  #   package = pkgs.swayidle;
+  #   events = [
+  #     {
+  #       event = "before-sleep";
+  #       command = "swaylock";
+  #     }
+  #     {
+  #       event = "lock";
+  #       command = "swaylock";
+  #     }
+  #   ];
+  #   timeouts = [{
+  #     timeout = 60;
+  #     command = "swaylock -fF";
+  #   }];
+  # };
 }
