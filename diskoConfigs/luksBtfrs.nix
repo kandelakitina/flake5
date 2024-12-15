6ba39c6346df79bdf6d1dc4a9f794e5a74c885fe
boticelli <kandelakitina@gmail.com>
2024-12-15 20:10:48 +0300

    Created a new luks disko file

--- diskoConfigs/luksBtfrs.nix
+++ diskoConfigs/luksBtfrs.nix
@@ -19,53 +19,52 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
             };
           };
-          swap = {
-            size = "4G";
-            content = {
-              type = "swap";
-              resumeDevice = true;
-            };
-          };
-          root = {
-            name = "root";
+          luks = {
             size = "100%";
+            label = "luks";
             content = {
-              type = "lvm_pv";
-              vg = "root_vg";
-            };
-          };
-        };
-      };
-    };
-    lvm_vg = {
-      root_vg = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "btrfs";
-              extraArgs = [ "-f" ];
-
-              subvolumes = {
-                "/root" = { mountpoint = "/"; };
-                # "/home" = {
-                #   mountpoint = "/home";
-                #   mountOptions = ["subvol=home" "noatime"];
-                # };
-                "/nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = [ "subvol=nix" "noatime" ];
-                };
-                "/persist" = {
-                  mountpoint = "/persist";
-                  mountOptions = [ "subvol=persist" "noatime" ];
-                };
-                "/log" = {
-                  mountpoint = "/var/log";
-                  mountOptions = [ "subvol=log" "noatime" ];
+              type = "luks";
+              name = "cryptroot";
+              extraOpenArgs = [
+                "--allow-discards"
+                "--perf-no_read_workqueue"
+                "--perf-no_write_workqueue"
+              ];
+              settings = {
+                crypttabExtraOpts = [ "fido2-device=auto" "token-timeout=10" ];
+              };
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-L" "nixos" "-f" ];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "subvol=root" "compress=zstd" "noatime" ];
+                  };
+                  # "/home" = {
+                  #   mountpoint = "/home";
+                  #   mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                  # };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "subvol=nix" "compress=zstd" "noatime" ];
+                  };
+                  "/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions =
+                      [ "subvol=persist" "compress=zstd" "noatime" ];
+                  };
+                  "/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [ "subvol=log" "compress=zstd" "noatime" ];
+                  };
+                  "/swap" = {
+                    mountpoint = "/swap";
+                    swap.swapfile.size = "64G";
+                  };
                 };
               };
             };
@@ -74,6 +73,7 @@
       };
     };
   };
+
   fileSystems."/persist".neededForBoot = true;
   fileSystems."/var/log".neededForBoot = true;
   fileSystems."/etc/ssh".neededForBoot = true;
