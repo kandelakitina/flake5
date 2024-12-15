83d8b3e0183636739f9e9c01e644249c53a8c8d3
boticelli <kandelakitina@gmail.com>
2024-12-15 18:41:23 +0300

    Updated sound window applet

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -162,8 +162,19 @@
           "layersOut,1,3,easeinback,slide"
         ];
       };
-
     };
 
+    extraConfig = # hyprland
+      ''
+        # Pipewire Volume Control
+        windowrulev2 = float, title:^(Pipewire Volume Control)$
+        windowrulev2 = size 50% 20%, title:^(Pipewire Volume Control)$
+        windowrulev2 = move 45% 10%, title:^(Pipewire Volume Control)$
+
+        # Blueman applet
+        windowrulev2 = float, title:^(Bluetooth Devices)$
+        windowrulev2 = size 50% 20%, title:^(Bluetooth Devices)$
+        windowrulev2 = move 45% 20%, title:^(Bluetooth Devices)$
+      '';
   };
 }
--- modules/home-manager/hyprland/waybar/settings.nix
+++ modules/home-manager/hyprland/waybar/settings.nix
@@ -142,7 +142,7 @@
       format-icons = { default = [ " " ]; };
       scroll-step = 5;
       on-click = "pamixer -t";
-      on-click-right = "pwvucontrol";
+      on-click-right = "kill pwvucontrol || pwvucontrol";
     };
     battery = {
       format = "{icon} {capacity}%";
