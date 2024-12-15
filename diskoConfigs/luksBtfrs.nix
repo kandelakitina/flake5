6c10abe81d8b1da4d98e7ca713eab40eb5c22c97
boticelli <kandelakitina@gmail.com>
2024-12-15 20:04:36 +0300

    Start experimenting with luks

---
+++ diskoConfigs/luksBtfrs.nix
@@ -0,0 +1,81 @@
+{ device ? throw "Set this to your disk device, e.g. /dev/sda", ... }: {
+  disko.devices = {
+    disk.main = {
+      inherit device;
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          swap = {
+            size = "4G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "root_vg";
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      root_vg = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+
+              subvolumes = {
+                "/root" = { mountpoint = "/"; };
+                # "/home" = {
+                #   mountpoint = "/home";
+                #   mountOptions = ["subvol=home" "noatime"];
+                # };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "subvol=nix" "noatime" ];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [ "subvol=persist" "noatime" ];
+                };
+                "/log" = {
+                  mountpoint = "/var/log";
+                  mountOptions = [ "subvol=log" "noatime" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
+  fileSystems."/etc/ssh".neededForBoot = true;
+  fileSystems."/var/lib/sops-nix".neededForBoot = true;
+}
