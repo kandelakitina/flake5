673a80dfd3710c4918c5f9739d7ea119829f2930
kandelakitina <108508127+kandelakitina@users.noreply.github.com>
2024-12-16 14:51:39 +0300

    Update impermanence.nix

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -3,52 +3,50 @@ let removeTmpFilesOlderThan = 14;
 in {
   imports = [ inputs.impermanence.nixosModules.impermanence ];
 
-boot.initrd.postDeviceCommands = ''
-  # Define root mountpoint
-  mkdir -p /btrfs_tmp
+    boot.initrd.systemd.services.rollback = {
+      description = "Rollback BTRFS root subvolume to a pristine state";
+      wantedBy = ["initrd.target"];
+      # make sure it's done after encryption
+      # i.e. LUKS/TPM process
+      after = ["systemd-cryptsetup@enc.service"];
+      # mount the root fs before clearing
+      before = ["sysroot.mount"];
+      unitConfig.DefaultDependencies = "no";
+      serviceConfig.Type = "oneshot";
+      script = ''
+        mkdir -p /mnt
 
-  # Wait for root device to be available
-  until [ -e /dev/mapper/cryptroot ]; do
-      echo "Waiting for /dev/mapper/cryptroot to be ready..."
-      sleep 1
-  done
+        # We first mount the btrfs root to /mnt
+        # so we can manipulate btrfs subvolumes.
+        mount -o subvol=/ /dev/mapper/enc /mnt
+        btrfs subvolume list -o /mnt/root
 
-  # Mount the root Btrfs filesystem
-  mount /dev/mapper/cryptroot /btrfs_tmp || {
-      echo "Failed to mount /dev/mapper/cryptroot"
-      exit 1
-  }
+        # While we're tempted to just delete /root and create
+        # a new snapshot from /root-blank, /root is already
+        # populated at this point with a number of subvolumes,
+        # which makes `btrfs subvolume delete` fail.
+        # So, we remove them first.
+        #
+        # /root contains subvolumes:
+        # - /root/var/lib/portables
+        # - /root/var/lib/machines
 
-  # Handle old root subvolume
-  if [[ -e /btrfs_tmp/root ]]; then
-      mkdir -p /btrfs_tmp/old_roots
-      timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%d_%H:%M:%S")
-      mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  fi
+        btrfs subvolume list -o /mnt/root |
+        cut -f9 -d' ' |
+        while read subvolume; do
+          echo "deleting /$subvolume subvolume..."
+          # btrfs subvolume delete "/mnt/$subvolume"
+        done &&
+        echo "deleting /root subvolume..." &&
+        # btrfs subvolume delete /mnt/root
 
-  # Recursive Btrfs subvolume deletion
-  delete_subvolume_recursively() {
-      IFS=$'\n'
-      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-          delete_subvolume_recursively "/btrfs_tmp/$i"
-      done
-      btrfs subvolume delete "$1"
-  }
+        echo "restoring blank /root subvolume..."
+        # btrfs subvolume snapshot /mnt/root-blank /mnt/root
 
-  # Clean up old roots older than the specified age
-  for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -type d -mtime +${builtins.toString removeTmpFilesOlderThan}); do
-      delete_subvolume_recursively "$i"
-  done
-
-  # Create a fresh root subvolume
-  btrfs subvolume create /btrfs_tmp/root || {
-      echo "Failed to create Btrfs root subvolume"
-      exit 1
-  }
-
-  # Unmount and cleanup
-  umount /btrfs_tmp
-'';
+        # Once we're done rolling back to a blank snapshot,
+        # we can unmount /mnt and continue on the boot process.
+        umount /mnt
+      '';
 
 
   environment.persistence."/persist" = {
