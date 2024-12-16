4336c80a548b8c0a09d3ae7c7f0a9f3b4ddcad94
kandelakitina <108508127+kandelakitina@users.noreply.github.com>
2024-12-16 15:30:18 +0300

    Update luksBtfrs.nix

--- diskoConfigs/luksBtfrs.nix
+++ diskoConfigs/luksBtfrs.nix
@@ -42,10 +42,10 @@
                       mountpoint = "/";
                       mountOptions = ["subvol=root" "compress=zstd" "noatime"];
                     };
-                    "/home" = {
-                      mountpoint = "/home";
-                      mountOptions = ["subvol=home" "compress=zstd" "noatime"];
-                    };
+                    # "/home" = {
+                    #   mountpoint = "/home";
+                    #   mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                    # };
                     "/nix" = {
                       mountpoint = "/nix";
                       mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
