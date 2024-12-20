0ebd2a600cf6d6fb6a773788d7f675d80086a138
boticelli <kandelakitina@gmail.com>
2024-12-20 16:42:39 +0300

    Updade SSH host options

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -14,5 +14,16 @@
       AllowStreamLocalForwarding no
       AuthenticationMethods publickey
     '';
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 }
