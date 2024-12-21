c6636f17f157ef064173353e49a1feab23121180
boticelli <kandelakitina@gmail.com>
2024-12-21 17:11:49 +0300

    Revert "try to update sops.nix"
    This reverts commit 68286e0db341678218fb2f6a3f4c551dbaf8568c.

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -14,16 +14,11 @@
 
     age = {
       # automatically import host SSH keys as age keys
-      sshKeyPaths = [
-        "/persist/etc/ssh/ssh_host_ed25519_key"
-        "/mnt/etc/ssh/ssh_host_ed25519_key"
-        # "/etc/ssh/ssh_host_ed25519_key"
-      ];
+      sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
       # this is where the age key derived from the above SSH will live
-      # keyFile = "/persist/var/lib/sops-nix/key.txt";
-      keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = "/persist/var/lib/sops-nix/key.txt";
       # generate a new key if the key specified above does not exist
-      generateKey = false;
+      generateKey = true;
     };
   };
 
