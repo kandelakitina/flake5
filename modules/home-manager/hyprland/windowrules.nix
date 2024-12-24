138ff0713e2e03dc832e02f251c55127dc2dbd13
boticelli <kandelakitina@gmail.com>
2024-12-24 20:02:38 +0300

    Update window rules for telegram and whatsapp

--- modules/home-manager/hyprland/windowrules.nix
+++ modules/home-manager/hyprland/windowrules.nix
@@ -12,6 +12,12 @@
         windowrulev2 = size 50% 20%, title:^(Bluetooth Devices)$
         windowrulev2 = move 45% 20%, title:^(Bluetooth Devices)$
 
+        # Telegram
+        windowrulev2 = workspace 4, title:Telegram*
+
+        # Whatsapp
+        windowrulev2 = workspace 4, title:^(WhatsApp for Linux)$
+
         # zk last scratchpad
         # windowrulev2 = float, move 50% 0,resize set 50% 100%,title:zklast
       '';
