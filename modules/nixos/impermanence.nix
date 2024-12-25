799c0ffea94d89b712c904788275f1cbf65daacf
boticelli <kandelakitina@gmail.com>
2024-12-25 14:06:28 +0300

    Update formatting of impermanence module

--- modules/nixos/impermanence.nix
+++ modules/nixos/impermanence.nix
@@ -3,52 +3,51 @@ let removeTmpFilesOlderThan = 14;
 in {
   imports = [ inputs.impermanence.nixosModules.impermanence ];
 
-    boot.initrd.systemd.services.rollback = {
-      description = "Rollback BTRFS root subvolume to a pristine state";
-      wantedBy = ["initrd.target"];
-      # make sure it's done after encryption
-      # i.e. LUKS/TPM process
-      after = ["systemd-cryptsetup@enc.service"];
-      # mount the root fs before clearing
-      before = ["sysroot.mount"];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        mkdir -p /mnt
+  boot.initrd.systemd.services.rollback = {
+    description = "Rollback BTRFS root subvolume to a pristine state";
+    wantedBy = [ "initrd.target" ];
+    # make sure it's done after encryption
+    # i.e. LUKS/TPM process
+    after = [ "systemd-cryptsetup@enc.service" ];
+    # mount the root fs before clearing
+    before = [ "sysroot.mount" ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir -p /mnt
 
-        # We first mount the btrfs root to /mnt
-        # so we can manipulate btrfs subvolumes.
-        mount -o subvol=/ /dev/mapper/enc /mnt
-        btrfs subvolume list -o /mnt/root
+      # We first mount the btrfs root to /mnt
+      # so we can manipulate btrfs subvolumes.
+      mount -o subvol=/ /dev/mapper/enc /mnt
+      btrfs subvolume list -o /mnt/root
 
-        # While we're tempted to just delete /root and create
-        # a new snapshot from /root-blank, /root is already
-        # populated at this point with a number of subvolumes,
-        # which makes `btrfs subvolume delete` fail.
-        # So, we remove them first.
-        #
-        # /root contains subvolumes:
-        # - /root/var/lib/portables
-        # - /root/var/lib/machines
+      # While we're tempted to just delete /root and create
+      # a new snapshot from /root-blank, /root is already
+      # populated at this point with a number of subvolumes,
+      # which makes `btrfs subvolume delete` fail.
+      # So, we remove them first.
+      #
+      # /root contains subvolumes:
+      # - /root/var/lib/portables
+      # - /root/var/lib/machines
 
-        btrfs subvolume list -o /mnt/root |
-        cut -f9 -d' ' |
-        while read subvolume; do
-          echo "deleting /$subvolume subvolume..."
-          # btrfs subvolume delete "/mnt/$subvolume"
-        done &&
-        echo "deleting /root subvolume..." &&
-        # btrfs subvolume delete /mnt/root
+      btrfs subvolume list -o /mnt/root |
+      cut -f9 -d' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        # btrfs subvolume delete "/mnt/$subvolume"
+      done &&
+      echo "deleting /root subvolume..." &&
+      # btrfs subvolume delete /mnt/root
 
-        echo "restoring blank /root subvolume..."
-        # btrfs subvolume snapshot /mnt/root-blank /mnt/root
+      echo "restoring blank /root subvolume..."
+      # btrfs subvolume snapshot /mnt/root-blank /mnt/root
 
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt and continue on the boot process.
-        umount /mnt
-      '';
+      # Once we're done rolling back to a blank snapshot,
+      # we can unmount /mnt and continue on the boot process.
+      umount /mnt
+    '';
   };
-
 
   environment.persistence."/persist" = {
     hideMounts = true;
@@ -68,7 +67,8 @@ in {
 
   system.activationScripts.persistent-dirs.text = let
     mkHomePersist = user:
-      lib.optionalString user.createHome ''
+      lib.optionalString user.createHome # sh
+      ''
         mkdir -p /persist/${user.home}
         chown ${user.name}:${user.group} /persist/${user.home}
         chmod ${user.homeMode} /persist/${user.home}
