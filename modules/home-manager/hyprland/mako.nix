1bf937b47d650ee15c76e5b0081fffee005fe9a4
boticelli <kandelakitina@gmail.com>
2025-05-29 14:36:12 +0300

    Flake update

--- modules/home-manager/hyprland/mako.nix
+++ modules/home-manager/hyprland/mako.nix
@@ -8,24 +8,24 @@ in {
 
   services = {
     mako = {
-      enable = true;
-      font = config.fontProfiles.regular.family;
-      padding = "15";
-      defaultTimeout = 5000;
-      borderSize = 2;
-      borderRadius = 5;
-      backgroundColor = "#${palette.base00}";
-      borderColor = "#${palette.base06}";
-      progressColor = "over #${palette.base0B}";
-      textColor = "#${palette.base0D}";
-      icons = true;
-      actions = true;
-      extraConfig = ''
-        text-alignment=center
-        [urgency=high]
-        default-timeout=100000000
-        border-color=#${palette.base09}
-      '';
+      settings = {
+        enable = true;
+        font = config.fontProfiles.regular.family;
+        padding = "15";
+        defaultTimeout = 5000;
+        borderSize = 2;
+        borderRadius = 5;
+        backgroundColor = "#${palette.base00}";
+        borderColor = "#${palette.base06}";
+        progressColor = "over #${palette.base0B}";
+        textColor = "#${palette.base0D}";
+        icons = true;
+        actions = true;
+        text-alignment = "center";
+        urgency = "high";
+        default-timeout = 100000000;
+        border-color = "#${palette.base09}";
+      };
     };
   };
 }
