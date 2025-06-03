86314eba7aefdd1d264fa1c1102875c44a94837e
boticelli <kandelakitina@gmail.com>
2025-06-03 11:03:19 +0300

    Finish ruby config for now

--- modules/home-manager/ruby.nix
+++ modules/home-manager/ruby.nix
@@ -2,9 +2,15 @@
   home.packages = with pkgs;
     with rubyPackages; [
       ruby
-      # rubyfmt
-      # ruby-lsp
-      rubocop
+      pry-byebug
+
+      # formatters
+      # rubocop
+      rubyfmt
+
+      # LSP
       solargraph
+      # ruby-lsp
+      sorbet-runtime # type checker
     ];
 }
