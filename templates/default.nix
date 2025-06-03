64dbfe169979e8b6c09bd2f12c5e94ccc5c1fd36
boticelli <kandelakitina@gmail.com>
2025-06-04 00:08:46 +0300

    Update ruby template 2

--- templates/default.nix
+++ templates/default.nix
@@ -4,4 +4,8 @@ rec {
     path = ./basic;
     description = "Minimal Flakelight flake.";
   };
+  ruby = {
+    path = ./ruby;
+    description = "Ruby dev env with gemfile, bundler, bundlix, etc.";
+  };
 }
