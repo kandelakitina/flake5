5e38c90ab01e09456258e95df827cda49ffd212b
boticelli <kandelakitina@gmail.com>
2025-06-21 18:15:00 +0300

    Switch aichat to OpenAI

--- modules/home-manager/aichat.nix
+++ modules/home-manager/aichat.nix
@@ -1,31 +1,38 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+let
+  ai = pkgs.writeShellScriptBin "ai" # bash
+    ''
+      #!/bin/sh
+      aichat "$@"
+    '';
 
-  home.packages = with pkgs; [ aichat ];
-  # xdg.configFile."aichat/config.yaml".text = # yaml
-  #   ''
-  #     model: openai:gpt-4o
-  #     clients:
-  #     - type: openai
-  #       # api_key comes from $OPENAI_API_KEY env var
-  #       # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
-  #       api_key: null
-  #     stream: true
-  #     save: true
-  #     keybindings: v
-  #   '';
+in {
+  home.packages = with pkgs; [ ai aichat ];
   xdg.configFile."aichat/config.yaml".text = # yaml
     ''
-      model: codestral:codestral-latest
-
+      model: openai:gpt-4o
       clients:
-      - type: openai-compatible
-        name: codestral
-        api_base: https://codestral.mistral.ai/v1/
+      - type: openai
+        # api_key comes from $OPENAI_API_KEY env var
+        # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
         api_key: null
-        # set it in env var MISTRAL_API_KEY
-
       stream: true
       save: true
       keybindings: v
     '';
+  # xdg.configFile."aichat/config.yaml".text = # yaml
+  #   ''
+  #     model: codestral:codestral-latest
+
+  #     clients:
+  #     - type: openai-compatible
+  #       name: codestral
+  #       api_base: https://codestral.mistral.ai/v1/
+  #       api_key: null
+  #       # set it in env var MISTRAL_API_KEY
+
+  #     stream: true
+  #     save: true
+  #     keybindings: v
+  #   '';
 }
