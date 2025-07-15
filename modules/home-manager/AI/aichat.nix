cde8173c9708dc5831601b1585bf8249c1e1135b
boticelli <kandelakitina@gmail.com>
2025-07-15 13:22:42 +0300

    Add mistral ai

--- modules/home-manager/AI/aichat.nix
+++ modules/home-manager/AI/aichat.nix
@@ -9,6 +9,9 @@ let
 
 in {
   home.packages = with pkgs; [ ai aichat ];
+
+  # OPENAI
+  # ------
   # xdg.configFile."aichat/config.yaml".text = # yaml
   #   ''
   #     model: openai:gpt-4o
@@ -21,16 +24,36 @@ in {
   #     save: true
   #     keybindings: v
   #   '';
+
+  # CODESTRAL
+  # ---------
+  # xdg.configFile."aichat/config.yaml".text = # yaml
+  #   ''
+  #     model: codestral:codestral-latest
+
+  #     clients:
+  #     - type: openai-compatible
+  #       name: codestral
+  #       api_base: https://codestral.mistral.ai/v1/
+  #       api_key: null
+  #       # set it in env var MISTRAL_API_KEY
+
+  #     stream: true
+  #     save: true
+  #     keybindings: v
+  #   '';
+
+  # MISTRAL
+  # ---------
   xdg.configFile."aichat/config.yaml".text = # yaml
     ''
-      model: codestral:codestral-latest
+      model: mistral
 
       clients:
       - type: openai-compatible
-        name: codestral
-        api_base: https://codestral.mistral.ai/v1/
-        api_key: null
-        # set it in env var MISTRAL_API_KEY
+        name: mistral
+        api_base: https://api.mistral.ai/v1
+        # api_key is defined in MISTRAL_API_KEY variable
 
       stream: true
       save: true
