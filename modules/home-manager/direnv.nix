7c68d3097b709ee46572cc9ab5238aaff808a86a
boticelli <kandelakitina@gmail.com>
2025-09-09 21:19:34 +0300

    Update direnv module

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -1,1 +1,7 @@
-{ programs.direnv = { enable = true; }; }
+{ pkgs, ... }: {
+  # home.packages = with pkgs; [ direnv ];
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+}
