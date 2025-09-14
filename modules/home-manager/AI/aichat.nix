f8fe5e6eac487944ee6b708419917defddb04684
boticelli <kandelakitina@gmail.com>
2025-09-14 12:46:34 +0300

    Add opencode and update AI tools

--- modules/home-manager/AI/aichat.nix
+++ modules/home-manager/AI/aichat.nix
@@ -3,45 +3,45 @@ let
   ai = pkgs.writeShellScriptBin "ai" # bash
     ''
       #!/bin/sh
-      proxychains4 -q aichat "$@"
-      # aichat "$@"
+      # proxychains4 -q aichat "$@"
+      aichat "$@"
     '';
 
 in {
   home.packages = with pkgs; [ ai aichat ];
 
-  # OPENAI
-  # ------
-  xdg.configFile."aichat/config.yaml".text = # yaml
-    ''
-      model: openai:gpt-4o
-      clients:
-      - type: openai
-        # api_key comes from $OPENAI_API_KEY env var
-        # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
-        api_key: null
-      stream: true
-      save: true
-      keybindings: v
-    '';
-
-  # CODESTRAL
-  # ---------
+  # # OPENAI
+  # # ------
   # xdg.configFile."aichat/config.yaml".text = # yaml
   #   ''
-  #     model: codestral:codestral-latest
-
+  #     model: openai:gpt-4o
   #     clients:
-  #     - type: openai-compatible
-  #       name: codestral
-  #       api_base: https://codestral.mistral.ai/v1/
+  #     - type: openai
+  #       # api_key comes from $OPENAI_API_KEY env var
+  #       # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
   #       api_key: null
-  #       # set it in env var MISTRAL_API_KEY
-
   #     stream: true
   #     save: true
   #     keybindings: v
   #   '';
+
+  # CODESTRAL
+  # ---------
+  xdg.configFile."aichat/config.yaml".text = # yaml
+    ''
+      model: codestral:codestral-latest
+
+      clients:
+      - type: openai-compatible
+        name: codestral
+        api_base: https://codestral.mistral.ai/v1/
+        api_key: null
+        # set it in env var MISTRAL_API_KEY
+
+      stream: true
+      save: true
+      keybindings: v
+    '';
 
   # MISTRAL
   # ---------
