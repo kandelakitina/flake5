3513cc5a7e5ba013c396e5fd523d1cdce59766fa
boticelli <kandelakitina@gmail.com>
2025-09-15 09:23:32 +0300

    Update opencode

--- modules/home-manager/AI/opencode.nix
+++ modules/home-manager/AI/opencode.nix
@@ -1,1 +1,11 @@
-{ pkgs, ... }: { home.packages = [ pkgs.opencode ]; }
+{
+  programs.opencode = {
+    enable = true;
+    settings = {
+      theme = "system";
+      # Get the models' names at https://models.dev
+      model = "opencode/grok-code";
+      small_model = "mistral/devstral-small-2507";
+    };
+  };
+}
