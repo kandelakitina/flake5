cb049ef433039f3d8ff9023cbc6775c874a272c5
boticelli <kandelakitina@gmail.com>
2025-09-15 12:59:29 +0300

    Revert "Update ruby version in rails template"
    This reverts commit f095e94e5e6fb00ff19fc63134f1089830d7038d.

--- templates/rails/flake.nix
+++ templates/rails/flake.nix
@@ -39,7 +39,7 @@
         gemConfig = { };
 
         # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
-        ruby = pkgs."ruby-3.4.3";
+        ruby = pkgs."ruby-3.3.1";
 
         # Running bundix would regenerate `gemset.nix`
         bundixcli = bundix.packages.${system}.default;
