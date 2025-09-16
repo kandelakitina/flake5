a683ffdfff5f549e210836abe058529cd13826a6
boticelli <kandelakitina@gmail.com>
2025-09-16 18:11:59 +0300

    Update ruby to 3_4

--- templates/rails/flake.nix
+++ templates/rails/flake.nix
@@ -39,7 +39,7 @@
         gemConfig = { };
 
         # See available versions here: https://github.com/bobvanderlinden/nixpkgs-ruby/blob/master/ruby/versions.json
-        ruby = pkgs.ruby_3_1;
+        ruby = pkgs.ruby_3_4;
 
         # Running bundix would regenerate `gemset.nix`
         bundixcli = bundix.packages.${system}.default;
