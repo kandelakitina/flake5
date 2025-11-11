6c198e9efd135fca2cf5932a7b504a5819cbc722
boticelli <kandelakitina@gmail.com>
2025-11-11 08:27:36 +0000

    update minisforum default

--- hosts/minisforum/default.nix
+++ hosts/minisforum/default.nix
@@ -1,7 +1,6 @@
 { inputs, outputs, lib, config, pkgs, ... }: {
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
-    inputs.hardware.nixosModules.common-ssd
     # inputs.hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
 
     # Import your generated (nixos-generate-config) hardware configuration
