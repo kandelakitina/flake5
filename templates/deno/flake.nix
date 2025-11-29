4c0594c8afd5d6c4182cb8509fe3516dc426c50c
boticelli <kandelakitina@gmail.com>
2025-11-29 16:45:24 +0300

    Update deno template

--- templates/deno/flake.nix
+++ templates/deno/flake.nix
@@ -5,12 +5,16 @@
   outputs = { flakelight, ... }:
     flakelight ./. {
       devShell = pkgs: {
-        packages = with pkgs; [ just live-server ];
+        packages = with pkgs; [
+          just
+          live-server # rust live-server
+          # posting # TUI API client
+        ];
         shellHook = ''
           export PATH="/home/boticelli/.deno/bin:$PATH"
 
-          echo -e "\033[32mdeno \`${pkgs.deno}/bin/deno --version\`\033[0m"
-          echo -e "\033[33mRun live-server for live-server\033[0m"
+          deno --version
+          echo -e "This shell provides: live-server, posting"
         '';
       };
     };
