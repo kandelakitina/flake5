3266f263fc25ab0f5328c1c9c7d7d4e5d9c2d44d
boticelli <kandelakitina@gmail.com>
2026-01-16 14:26:21 +0300

    Update opencode to work with mistral

--- modules/home-manager/AI/opencode.nix
+++ modules/home-manager/AI/opencode.nix
@@ -4,8 +4,9 @@
     settings = {
       theme = "system";
       # Get the models' names at https://models.dev
-      model = "opencode/grok-code";
-      small_model = "mistral/devstral-small-2507";
+      # model = "opencode/grok-code";
+      model = "mistral/codestral-latest";
+      # small_model = "mistral/devstral-small-2507";
     };
   };
 }
