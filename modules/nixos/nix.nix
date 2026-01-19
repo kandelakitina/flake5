e3017a3735fa986fe6271b758c4dad6309c8e224
kandelakitina <108508127+kandelakitina@users.noreply.github.com>
2026-01-19 10:11:08 +0300

    Update substituters in nix.nix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -28,17 +28,19 @@
       warn-dirty = false;
       system-features = [ "kvm" "big-parallel" "nixos-test" ];
 
+      substituters = ["https://aseipp-nix-cache.global.ssl.fastly.net"];
+
       # adding cachix servers for quick binary download
-      trusted-substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://nixpkgs-ruby.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM="
-      ];
+      # trusted-substituters = [
+      #   "https://cache.nixos.org"
+      #   "https://nix-community.cachix.org"
+      #   "https://nixpkgs-ruby.cachix.org"
+      # ];
+      # trusted-public-keys = [
+      #   "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      #   "nixpkgs-ruby.cachix.org-1:vrcdi50fTolOxWCZZkw0jakOnUI1T19oYJ+PRYdK4SM="
+      # ];
     };
 
     gc = {
